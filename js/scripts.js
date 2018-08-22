dae717342f0b963e10b4496f75112322e0d8b0bd
Brian Nelson and Vera Weikel <vera@>
2018-08-22 08:19:16 -0700

    pass spec and return turn over if the roll is a 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
+
+
+var evalDice = function(input) {
+  var output;
+  if (input === 1) {
+    output = "Your Turn is Over."
+  } else {
+    output = input;
+  }
+  return output;
+}
+
+
+
+
+
 $(document).ready(function() {
 
 
   $('#rollDice').click(function() {
     var roll = Math.ceil(Math.random()*6);
+    var output = evalDice(roll);
 
-
-    $('.output').append(roll);
+    $('.output').append(output + '<br>');
   });
 
 
